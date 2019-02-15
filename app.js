ec992a46474b7068029f7dfb4d4d19632c5deffd
mahta73 <techandcodeaddict@gmail.com>
2019-02-16 00:56:31 +0400

    first lesson

---
+++ app.js
@@ -0,0 +1,35 @@
+const fs = require('fs'); // buit in module
+const os = require('os');
+
+// fs.unlink -> delete a file
+// fs.unlink('./delete.js', err => {
+//     if (err) throw err;
+//     console.log('successfully deleted');
+// });
+
+// try {
+//     fs.unlinkSync('./delete2.js');
+//     console.log('successfully deleted');
+// } catch(err) {
+//     throw err;
+// }
+
+// fs.rename('./name.js' , './Name.js', err => {
+//     if(err) throw err;
+//     console.log('renamed complete');
+//     fs.stat('./Name.js', (err, stats) => {
+//         if (err) throw err;
+//         console.log(JSON.stringify(stats));
+//     });
+// })
+
+// fs.readFile('./app.js', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+fs.appendFile('./name.js', 'maybe one day my dreams come true', err => {
+    if (err) throw err;
+    console.log('appened successfully');
+});
+ 
